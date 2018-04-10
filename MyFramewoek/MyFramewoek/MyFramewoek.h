d2f41cfa0507b877586e67a19a9e6819ff2ad3ba
NinhNM <ninhnm91@gmail.com>
2018-04-10 13:49:49 +0700

    finish our MyFramework

---
+++ MyFramewoek/MyFramewoek/MyFramewoek.h
@@ -0,0 +1,19 @@
+//
+//  MyFramewoek.h
+//  MyFramewoek
+//
+//  Created by NinhNM_ACV on 4/10/18.
+//  Copyright © 2018 JuniorStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFramewoek.
+FOUNDATION_EXPORT double MyFramewoekVersionNumber;
+
+//! Project version string for MyFramewoek.
+FOUNDATION_EXPORT const unsigned char MyFramewoekVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramewoek/PublicHeader.h>
+
+
---
+++ MyFramewoek/MyFramewoek/MyFramework.swift
@@ -0,0 +1,14 @@
+//
+//  MyFramework.swift
+//  MyFramewoek
+//
+//  Created by NinhNM_ACV on 4/10/18.
+//  Copyright © 2018 JuniorStudio. All rights reserved.
+//
+
+import Foundation
+public class MyFramewoek {
+    public class func doSomething() -> String {
+        return "I'm developer ios"
+    }
+}
